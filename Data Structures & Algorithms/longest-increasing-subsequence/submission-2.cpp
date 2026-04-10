f3eaf144b1d58619bcfd3da82ac780a50f2d8892
Mayank Gupta <112984875+meliodas137@users.noreply.github.com>
2026-04-10 14:41:09 -0700

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) dp[i] = max(dp[i], dp[j]);
+            }
+            dp[i] += 1;
+        }
+
+        int res = 1;
+        for(int i = 0; i < n; i++) res = max(res, dp[i]);
+
+        return res;
+
+    }
+};
